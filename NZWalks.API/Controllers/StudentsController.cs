1c953feeb9dc74ad5bd28c01a64741c7a865d879
Ryan Coole <ryancoole1@gmail.com>
2024-05-17 16:50:56 +0100

    Running and Calling ASP.NET Core Web API

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:7139/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: https://localhost:7139/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] {"Hannah","Holly","Jon","Richard","Ryan"};
+
+            return Ok(studentNames);
+        }
+    }
+}
