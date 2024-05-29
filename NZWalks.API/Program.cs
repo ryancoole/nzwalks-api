ba34022c9e48c96782a253dc3c2f2c0737a374d4
Ryan Coole <ryancoole1@gmail.com>
2024-05-29 22:56:09 +0100

    Serving Static Files Through ASPNET Core Web API

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -7,6 +7,7 @@ using NZWalks.API.Mappings;
 using NZWalks.API.Repositories;
 using System.Text;
 using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -98,8 +99,13 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
-
 app.UseAuthorization();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Images")),
+    RequestPath = "/Images"
+});
 
 app.MapControllers();
 
